959f4018f9ad354c5cbc9eeeed413b135e6185e1
fkrydev <fikri.afa289@gmail.com>
2023-07-13 12:39:45 +0800

    update controller

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -11,7 +11,7 @@ class PublicController extends Controller
 {
     public function get_folder_items($folder_id)
     {
-        $folder = Storage::where('item_id', $folder_id)->first();
+        $folder = Storage::where('item_id', $folder_id)->with('user')->first();
         if ($folder && $folder->type == 'folder') {
             if ($folder->access == 'public') {
                 return response([
